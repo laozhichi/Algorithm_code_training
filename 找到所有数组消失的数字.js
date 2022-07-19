f898220cc1166cc65227885261eac6bc01b783d5
laozhichi <109329950+laozhichi@users.noreply.github.com>
2022-07-19 15:14:07 +0800

    Create 找到所有数组消失的数字.js

---
+++ 找到所有数组消失的数字.js
@@ -0,0 +1,16 @@
+/*题目描述转到leetcode博客：https://leetcode.cn/problems/find-all-numbers-disappeared-in-an-array/*/
+/*源码如下*/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+      var array=[];
+      var length=nums.length;
+      for(var i=1;i<=length;i++)
+      {
+          if(!nums.includes(i))
+          array.push(i);
+      }
+      return array;
+};
